a73612e7fb296b5edc4283939fbc89b96cf5a794
jomarolycan <jomarolycan@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-04-11 01:31:20 +0000

    reescrevi o index e separei os templates dele
    inclui as classes de sessão do marco
    
    padronizei alguns nomes pra ficarem mais parecidos com os do livro

--- trunk/s_almoxarifado/index.php
+++ trunk/s_almoxarifado/index.php
@@ -1,12 +1,61 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+$sessao = new Sessao();
+
+function __autoload($classe)
+{
+	   $pastas = array('app.widgets', 'app.ado', 'app.model', 'app.control');
+	   foreach ($pastas as $pasta)
+	   {
+		      if (file_exists("{$pasta}/{$classe}.class.php"))
+                      {
+		
+			         include_once "{$pasta}/{$classe}.class.php";
+		      }
+	   }
+}
+
+
+include 'app.funcoes/validate.php';
+
+validate();
+
+
+
+class TApplication
+{
+	   static public function run()
+	   {
+                 
+                 if(!$_GET) $_GET['class'] = 'Principal';
+                 
+                 // carrega a pagina de login e pula o restante
+                 if($_GET['class'] == 'Login') include ('template/login.html');
+                 else
+                 {
+                      // abre o arquivo de template
+                      $template = file_get_contents('layout.html');
+		      
+                      // inicializa a variavel que vai armazenar o conteudo
+                      $content = '';
+
+                        $class = $_GET['class'];
+                        if (class_exists($class))
+                        {
+                                $pagina = new $class;
+                                ob_start();
+                                $pagina->show();
+                                $content = ob_get_contents();
+                                ob_end_clean();
+                        }
+                        else if (function_exists($method))
+                        {
+                                call_user_func($method, $_GET);
+                        }
+
+		      echo str_replace('#conteudo#', $content, $template);
+                 }
+	   }
+}
+TApplication::run();
+?>
---
+++ trunk/s_almoxarifado/testes/testePath.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$path = $_SERVER['SCRIPT_NAME'];
+
+var_dump($_SERVER);
+
+var_dump($path);
+
+
+?>
