493a4936ce1dd49719158c7962474fe805f7adfd
luancedup <luancedup@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-04-24 13:52:52 +0000

    haha

---
+++ trunk/s_almoxarifado/app.ado/SearchCriteria.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SearchCritera
+ *
+ * @author Home
+ */
+class SearchCriteria {
+    
+    private $value;
+    
+    function __construct(){
+        
+        
+    }
+    
+    
+    function setValueCriteria($value){
+        $this->value = $value;
+    }
+    
+    function getValueCriteria(){
+        return $this->value;
+    }
+}
+
+?>
