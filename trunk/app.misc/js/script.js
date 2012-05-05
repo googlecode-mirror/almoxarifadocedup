e27f0e6a9b77b77a9c69a3ea6598de1ffdaf620a
luancedup <luancedup@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-05-05 15:24:39 +0000

    começando com emprestimo.

--- trunk/app.misc/js/script.js
+++ trunk/app.misc/js/script.js
@@ -1,44 +1,3 @@
-/*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 2011 Oracle and/or its affiliates. All rights reserved.
- *
- * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
- * Other names may be trademarks of their respective owners.
- *
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common
- * Development and Distribution License("CDDL") (collectively, the
- * "License"). You may not use this file except in compliance with the
- * License. You can obtain a copy of the License at
- * http://www.netbeans.org/cddl-gplv2.html
- * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
- * specific language governing permissions and limitations under the
- * License.  When distributing the software, include this License Header
- * Notice in each file and include the License file at
- * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the GPL Version 2 section of the License file that
- * accompanied this code. If applicable, add the following below the
- * License Header, with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * If you wish your version of this file to be governed by only the CDDL
- * or only the GPL Version 2, indicate your decision by adding
- * "[Contributor] elects to include this software in this distribution
- * under the [CDDL or GPL Version 2] license." If you do not indicate a
- * single choice of license, a recipient has the option to distribute
- * your version of this file under either the CDDL, the GPL Version 2 or
- * to extend the choice of license to its licensees as provided above.
- * However, if you add GPL Version 2 code and therefore, elected the GPL
- * Version 2 license, then the option applies only if the new code is
- * made subject to such option by the copyright holder.
- *
- * Contributor(s):
- *
- * Portions Copyrighted 2011 Sun Microsystems, Inc.
- */
 
 $(document).ready(function() {
     initDatepicker();
@@ -48,14 +7,38 @@ $(document).ready(function() {
     initDeleteDialog();
     intAlertMsg();
     initFormDelete();
+    initFormDialogMat();
 });
 
 function initDatepicker() {
+    
+    $.datepicker.regional['pt-BR'] = {
+		closeText: 'Fechar',
+		prevText: '&#x3c;Anterior',
+		nextText: 'Pr&oacute;ximo&#x3e;',
+		currentText: 'Hoje',
+		monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho',
+		'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+		monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
+		'Jul','Ago','Set','Out','Nov','Dez'],
+		dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+		dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+		dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 0,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+    
+    $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
     $('.datepicker')
         .attr('readonly', 'readonly')
         .datepicker({
-            dateFormat: 'yy-m-d'
+            dateFormat: 'dd/mm/yy'
         });
+        
+    
 }
 
 function initFlashes() {
@@ -104,6 +87,32 @@ function initFormDialog() {
         autoOpen: false,
         modal: true,
         width: 520,
+        buttons: {
+            'OK': function() {
+                Form.submit();
+                $(this).dialog('close');
+            },
+            'Cancel': function() {
+                $(this).dialog('close');
+            }
+        }
+    });
+    Link.click(function() {
+    Form.attr('action', $(this).attr('href'));
+    Dialog.dialog('option', 'title', $(this).attr('title'));
+    Dialog.dialog('open');
+        return false;
+    });
+}
+
+function initFormDialogMat() {
+    var Dialog = $('#janela-dialogMat');
+    var Link = $('.open-linkMat');
+    var Form = $('#form-dialogMat');
+    Dialog.dialog({
+        autoOpen: false,
+        modal: true,
+        width: Dialog.attr('width'),
         buttons: {
             'OK': function() {
                 Form.submit();
