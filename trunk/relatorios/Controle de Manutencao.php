1ceb60b3009c1578ca94cceb332a9f82ca92658b
jomarolycan <jomarolycan@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-05-06 21:49:36 +0000

    colocando o novo menu
    implementando o styleLink e scriptLink

---
+++ trunk/relatorios/Controle de Manutencao.php
@@ -0,0 +1,23 @@
+<table>
+	<thead>
+		<tr>
+			<th>Laboratório: </th>
+			<th>Equipamento: </th>
+			<th>Defeito: </th>
+			<th>Data: __/__/____</th>
+			<th>Requisitante: </th>
+		</tr>
+		<tr>
+			<th>Data: __/__/____</th>
+			<th>Providencia</th>
+			<th>Responsável</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
