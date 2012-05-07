8b5c19a606df12997eda97c6484eaa93896f1fd2
jomarolycan <jomarolycan@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-05-07 12:47:02 +0000

    sei la - jomaro

--- trunk/app.functions/buscaPermissoes.php
+++ trunk/app.functions/buscaPermissoes.php
@@ -11,13 +11,17 @@ inner join modulos_permissoes_usuarios mpu on (mpu.modulo_permissao_id = mp.id_m
 where mpu.usuario_id = '.$id.'
 group by m.nome_modulo;')->fetchAll(PDO::FETCH_COLUMN);
     
-    foreach($modulos as $modulo)
+    foreach($modulos as $modulo){
     $permissoes[$modulo] = $db->getConn()->query('select p.nome_permissao from permissoes p
 inner join modulos_permissoes mp on (mp.permissao_id = p.id_permissao)
 inner join modulos m on (mp.modulo_id = m.id_modulo)
 inner join modulos_permissoes_usuarios mpu on (mpu.modulo_permissao_id = mp.id_modulo_permissao)
 where mpu.usuario_id = '.$id.' and m.nome_modulo like "'.$modulo.'"
 group by p.nome_permissao;')->fetchAll(PDO::FETCH_COLUMN);
+    }
+    
+    $permissoes['usuarios'][] = 'info-usuario';
+    $permissoes['manutencoes'][] = 'm-manutencoes';
     
     return $permissoes;
 }
