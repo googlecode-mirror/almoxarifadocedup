b913af30e9d3c9f1300032f93b92096d340ce718
luancedup <luancedup@gmail.com>
2012-05-07 13:33:09 +0000

    Ah sei la

--- modulos/chaves/app.control/visualizar.php
+++ modulos/chaves/app.control/visualizar.php
@@ -1,3 +1,14 @@
 <?php
     $laboratorios = LabMapper::getLabs();
+    
+    if (array_key_exists('labkey',$_GET)){
+    	
+    	$page = 'chave-detail';
+    
+	    $ch = Utils::findById($_GET['labkey'], 'ctrl_chaves', 'laboratorio_id');
+	    $lab = Utils::findById($_GET['labkey'], 'laboratorios', 'id_laboratorio');
+	    $user = Utils::findById($ch['professor_id'], 'usuarios', 'id_usuario');
+	    $data_hora = explode(' ',$ch['dt_inicial_controle']);
+
+    }
 ?>
