f7aef6e996673c99e59926f7f1fd3c54dcd52208
luancedup@gmail.com <luancedup@gmail.com@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-05-12 00:53:52 +0000

    começando com validação

--- trunk/modulos/usuarios/app.control/gerenciar.php
+++ trunk/modulos/usuarios/app.control/gerenciar.php
@@ -11,15 +11,33 @@
 
   
    if (array_key_exists('edit',$_POST)){
+   	
+   	$dados = array('nome_usuario' => array('Nome'),
+   				   'email_usuario'=> array('Email', 'tipo' => 'email'),
+   			       'telefone_usuario' => array ('Telefone', 'tipo' => 'inteiro'),
+   				   'dt_nascimento_usuario' => array ('Data Nascimento', 'tipo' => 'data'),
+   				   'login_usuario' => array ("Login",'tipo' =>'nome'),
+   				   'senha_usuario' => array('Senha'),
+   			       'confirma_senha' => array('Repita a Senha', 'tipo' => 'igualdade', 'compara' => 'senha_usuario', 'legenda_2' => 'Senha'),
+   					);   
+   	
+   	
+   	$validacao = ValidaFormulario($dados);
+   	
+   	if ($validacao === true){
       
-      $objectUsuario = new Usuario();
-      $objectUsuario->setIdUsuario($id);
-      
-      UsuarioMapper::map($objectUsuario,$_POST);
-      UsuarioMapper::update($objectUsuario);
-      
-      $sessao->addVar('msg',1);
-      header('location:index.php?modulo=usuarios&page=visualizar');
+	      $objectUsuario = new Usuario();
+	      $objectUsuario->setIdUsuario($id);
+	      
+	      UsuarioMapper::map($objectUsuario,$_POST);
+	      UsuarioMapper::update($objectUsuario);
+	      
+	      $sessao->addVar('msg',1);
+	      header('location:index.php?modulo=usuarios&page=visualizar');
+
+   	}else{
+   		Flash::addFlash($validacao);
+   	}
       
   }
   
--- trunk/modulos/usuarios/app.control/info-usuario.php
+++ trunk/modulos/usuarios/app.control/info-usuario.php
@@ -3,14 +3,30 @@
    $usuario = new Usuario(); 
 
    if (array_key_exists('edit',$_POST)){
+   
+	  $dados = array('nome_usuario' => array('Nome'),
+	   			'email_usuario'=> array('Email', 'tipo' => 'email'),
+	   			'telefone_usuario' => array ('Telefone', 'tipo' => 'inteiro'),
+	   			'dt_nascimento_usuario' => array ('Data Nascimento', 'tipo' => 'data'),
+	   			'login_usuario' => array ("Login",'tipo' =>'nome'),
+	   			'senha_usuario' => array('Senha'),
+	   			'confirma_senha' => array('Repita a Senha', 'tipo' => 'igualdade', 'compara' => 'senha_usuario', 'legenda_2' => 'Senha'),
+	  );
+	   	
+	  $validacao = ValidaFormulario($dados);
       
+	  if ($validacao === true){
       
-      $usuario->setIdUsuario($id);
-      
-      UsuarioMapper::map($usuario,$_POST);
-      UsuarioMapper::update($usuario);
-      
-      Flash::addFlash('Registro Alterado');  
+	      $usuario->setIdUsuario($id);
+	      
+	      UsuarioMapper::map($usuario,$_POST);
+	      UsuarioMapper::update($usuario);
+	      
+	      Flash::addFlash('Registro Alterado');
+	  
+	  }else{
+	      	Flash::addFlash($validacao);
+	  }  
      
   }
 
