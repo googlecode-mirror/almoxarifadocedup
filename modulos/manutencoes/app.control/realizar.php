c027b0a5d71e761caed980a92bac2d85f65e83b9
luancedup <luancedup@fd2d2ae7-d2b0-d956-9ee8-912598c4fe82>
2012-05-17 12:34:37 +0000

    sei la

--- modulos/manutencoes/app.control/realizar.php
+++ modulos/manutencoes/app.control/realizar.php
@@ -13,39 +13,37 @@ $validacao = ValidaFormulario($dados);
 
 if ($validacao === true){
 
-	if (array_key_exists('first',$_GET)){
+	if (array_key_exists('key',$_GET)){
 	   
 	    
 	    $data = array('responsavel_id' => $sessao->getVar('usuario')->id_usuario,
 	    	          'data_manutencao' => date('Y-m-d H:i:s'),
 	                  'definitivo_manutencao' => $_POST['definitivo_manutencao'],
-                            'providencia_manutencao' => $_POST['providencia_manutencao'],
+                          'providencia_manutencao' => $_POST['providencia_manutencao'],
 	                  'req_manutencao_id' => $id);
 	    
 	        $manutencao = new Manu();
 	        ManuMapper::map($manutencao,$data);
 	        ManuMapper::addManu($manutencao);
-	        ManuMapper::addProvidencia($manutencao);
 	    
             $sessao->addVar('msg','2');
 	    header('location:index.php?modulo=manutencoes&page=visualizar&key='.$id);
 	
-	   
-	}else{
-		
-		$data = array('responsavel_id' => $sessao->getVar('usuario')->id_usuario,
-				'data_manutencao' => date('Y-m-d H:i:s'),
-				'definitivo_manutencao' => $_POST['definitivo_manutencao'],
-				'providencia_manutencao' => $_POST['providencia_manutencao'],
-				'req_manutencao_id' => $id);
-		
-		$manutencao = new Manu();
-		ManuMapper::map($manutencao,$data);
-		ManuMapper::addProvidencia($manutencao);
-		
-                $sessao->addVar('msg','2');
-		header('location:index.php?modulo=manutencoes&page=visualizar&key='.$id);	
-	}
+        }  
+//	else{
+//		
+//		$data = array('responsavel_id' => $sessao->getVar('usuario')->id_usuario,
+//				'data_manutencao' => date('Y-m-d H:i:s'),
+//				'definitivo_manutencao' => $_POST['definitivo_manutencao'],
+//				'providencia_manutencao' => $_POST['providencia_manutencao'],
+//				'req_manutencao_id' => $id);
+//		
+//		$manutencao = new Manu();
+//		ManuMapper::map($manutencao,$data);
+//		
+//                $sessao->addVar('msg','2');
+//		header('location:index.php?modulo=manutencoes&page=visualizar&key='.$id);	
+//	}
 }
 
 ?>
